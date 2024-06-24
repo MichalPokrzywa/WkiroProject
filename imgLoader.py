97e777e29732f4e72a9e0c2eac544f99e50fa034
MichalPokrzywa <michpok467@student.polsl.pl>
2024-06-24 11:47:14 +0200

    fixed this function

--- imgLoader.py
+++ imgLoader.py
@@ -4,11 +4,14 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import cpu_count
 import time
+
+
 def load_images_from_folder(folder, scale, resize):
     images = []
     start_time = time.time()  # Start timing
     try:
-        filenames = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+        filenames = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f.lower().endswith(
+            ('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
         with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
             results = executor.map(lambda f: process_image(os.path.join(folder, f), scale, resize), filenames)
             for result in results:
@@ -20,6 +23,7 @@ def load_images_from_folder(folder, scale, resize):
     print(f"Time taken to load images: {end_time - start_time:.2f} seconds")
     return images
 
+
 def process_image(path, scale, resize):
     try:
         img = cv2.imread(path)
@@ -27,14 +31,15 @@ def process_image(path, scale, resize):
             return None
         height, width, channels = img.shape
         if resize:
-            img_resize = cv2.resize(img, (int(width / scale), int(height / scale)))
+            img_resize = cv2.resize(img, (0, 0), fx=float(1 / scale), fy=float(1 / scale), interpolation=cv2.INTER_AREA)
         else:
             img_resize = img
-        # img_normalize = normalize_image(img_resize)
-        return img_resize
+        img_normalize = normalize_image(img_resize)
+        return img_normalize
     except Exception as e:
         print(f"Error processing image {path}: {e}")
         return None
+
 
 def images_to_pixels(images):
     try:
@@ -43,6 +48,7 @@ def images_to_pixels(images):
         print(f"Error converting images to pixels: {e}")
         return []
 
+
 def normalize_image(image):
     ycbcr_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
     Y, Cr, Cb = cv2.split(ycbcr_image)
@@ -50,6 +56,7 @@ def normalize_image(image):
     ycbcr_image_eq = cv2.merge([Y_eq, Cr, Cb])
     normalized_image = cv2.cvtColor(ycbcr_image_eq, cv2.COLOR_YCrCb2BGR)
     return normalized_image
+
 
 def normalize_pixel_values(image):
     return image / 255.0
